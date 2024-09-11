c3a2d5d87497ee3bc678484c1b2195ba780b3743
Ayahaldomi <ayahaldomi@gmail.com>
2024-09-11 18:00:14 +0300

    s

--- E-Commerce/Back-End/E-Commerce/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
+++ E-Commerce/Back-End/E-Commerce/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c59d2cbdc3d93cbe3faeb7f2daee2466ffc42081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc7468d98b6f754763ffcb02b33f370b5cba762")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
